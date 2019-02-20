eaaeb9274e16faf997ff0350d9116ec92ff248f6
joyytoo <joytoo97@gmail.com>
2019-02-20 13:30:42 +0300

    Add bad words middleware

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_ARTICLE } from "../constants/action-types";
+import { FOUND_BAD_WORD } from "../constants/action-types";
 
 const initialState = {
   articles: []
@@ -9,6 +10,9 @@ function rootReducer(state = initialState, action) {
     return Object.assign({}, state, {
       articles: state.articles.concat(action.payload)
     });
+  }
+  if (action.type === FOUND_BAD_WORD) {
+    console.log("Bad word found")
   }
   return state;
 };
