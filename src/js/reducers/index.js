7cd5ebe813797a83966de82fb389c3b8916e925d
joyytoo <joytoo97@gmail.com>
2019-02-21 14:13:08 +0300

    Add asycn action

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,9 +1,11 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 import { FOUND_BAD_WORD } from "../constants/action-types";
+import { DATA_LOADED } from "../constants/action-types";
 
 const initialState = {
-  articles: []
-};
+  articles: [],
+  remoteArticles: []
+}
 
 function rootReducer(state = initialState, action) {
   if (action.type === ADD_ARTICLE) {
@@ -13,6 +15,11 @@ function rootReducer(state = initialState, action) {
   }
   if (action.type === FOUND_BAD_WORD) {
     console.log("Bad word found")
+  }
+  if (action.type === DATA_LOADED) {
+    return Object.assign({}, state, {
+      remoteArticles: state.remoteArticles.concat(action.payload)
+    });
   }
   return state;
 };
