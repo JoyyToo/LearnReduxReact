a7f9621c035214e2c5e06ee4324555deac32b6a6
joyytoo <joytoo97@gmail.com>
2019-02-21 14:41:57 +0300

    Implement redux saga

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,6 @@
 import { ADD_ARTICLE } from "../constants/action-types";
 import { FOUND_BAD_WORD } from "../constants/action-types"
+import { DATA_REQUESTED } from "../constants/action-types"
 
 export function addArticle(payload) {
     return { type: ADD_ARTICLE, payload }
@@ -10,11 +11,5 @@ export function foundBadWord(payload) {
 };
 
 export function getData() {
-  return function(dispatch) {
-    return fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(json => {
-        dispatch({ type: "DATA_LOADED", payload: json });
-      });
-  };
+  return { type: DATA_REQUESTED };
 }
